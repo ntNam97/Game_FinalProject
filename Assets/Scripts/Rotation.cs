bceb052a8cf29bc8bd477be8ccf630b1b90014af
ntNam97 <1512339@student.hcmus.edu.vn>
2019-06-20 22:11:23 +0700

    first commit

---
+++ Assets/Scripts/Rotation.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject character;
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void rotateLeft()
+    {
+        character.transform.Rotate(Vector3.up * 500.0f * Time.deltaTime);
+    }
+    public void rotateRight()
+    {
+        character.transform.Rotate(Vector3.up * (-500.0f) * Time.deltaTime);
+    }
+}

17e294b5c456adba16c34bf4c6cfb51fa62b743e
phtrungit <phtrungit@gmail.com>
2019-06-28 20:39:24 +0700

    about feature

---
+++ Assets/AboutUs.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AboutUs : MonoBehaviour {
+
+	public void ShowAbout()
+    {
+        if (gameObject.transform.Find("About"))
+        {
+            bool status = gameObject.transform.Find("About").gameObject.activeSelf;
+            Debug.Log("found: "+!status);
+            gameObject.transform.Find("About").gameObject.SetActive(!status);
+        }
+        else
+            Debug.Log("Not found");
+       
+    }
+    public void HideAbout()
+    {
+        if (gameObject.transform.Find("About"))
+        {
+            Debug.Log("found");
+            gameObject.transform.Find("About").gameObject.SetActive(false);
+        }
+        else
+            Debug.Log("Not found");
+
+    }
+}
